fd84d26a26de2c134de8812b9720790ff246c163
shashwathuchil <shashwathamruthuchil@gmail.com>
2025-07-06 01:54:34 +0530

    Refactor svelte.config.js for improved readability and maintainability

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-node";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+	    paths: {
+      	base: '/e-commerce-SvelteKit' // Adjust this path based on your deployment setup
+    }
+  },
 };
 
 export default config;
