2eba0dec535b3cfb05b1cf04b502aae38fb01cf4
shashwathuchil <shashwathamruthuchil@gmail.com>
2025-07-06 01:25:01 +0530

    Add type definitions for Product and CartItem in stores.js

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,7 +1,22 @@
 import { writable } from 'svelte/store';
 
+/**
+ * @typedef {Object} Product
+ * @property {number} id
+ * @property {string} name
+ * @property {number} price
+ * @property {string} image
+ */
+
+/**
+ * @typedef {Product & { quantity?: number }} CartItem
+ */
+
+/** @type {import('svelte/store').Writable<Product[]>} */
 export const featuredProducts = writable([
   { id: 1, name: 'Wireless Headphones', price: 99.99, image: '/images/headphones.jpg' },
   { id: 2, name: 'Smart Watch', price: 149.99, image: '/images/smartwatch.jpg' },
   { id: 3, name: 'Bluetooth Speaker', price: 59.99, image: '/images/speaker.jpg' }
 ]);
+/** @type {import('svelte/store').Writable<CartItem[]>} */
+export const cart = writable([]);
